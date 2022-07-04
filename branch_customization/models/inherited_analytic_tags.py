cc62ee47712fd4d0e01d7974a82e6edb629df608
smcviltco <75363152+smcviltco@users.noreply.github.com>
2022-07-04 12:18:12 +0500

    a

--- branch_customization/models/inherited_analytic_tags.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models, _
-
-
-class AnalyticalAccountTags(models.Model):
-    _inherit = "account.analytic.tag"
-
-    branch_id = fields.Many2one('res.branch', string="Branch", readonly=True)
